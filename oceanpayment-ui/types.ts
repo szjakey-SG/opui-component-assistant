0e134092902a926156a3bfabe73b8e916db65eb4
szjakey-SG <szjakey@gmail.com>
2026-06-11 08:58:36 +0800

    Add oceanpayment-ui/types.ts

---
+++ oceanpayment-ui/types.ts
@@ -0,0 +1,126 @@
+export type {
+    AffixProps,
+    AnchorProps,
+    AnchorLinkProps,
+    AutoCompleteProps,
+    AlertProps,
+    AvatarProps,
+    BackTopProps,
+    BadgeProps,
+    BreadcrumbProps,
+    BreadcrumbItemProps,
+    BreadcrumbSeparatorProps,
+    ButtonProps,
+    CalendarProps,
+    CardProps,
+    CollapseProps,
+    CollapsePanelProps,
+    CarouselProps,
+    CascaderProps,
+    CheckboxProps,
+    CheckboxGroupProps,
+    CheckboxOptionType,
+    ColProps,
+    CommentProps,
+    ConfigProviderProps,
+    DatePickerProps,
+    DescriptionsProps,
+    DividerProps,
+    DropdownProps,
+    DrawerProps,
+    EmptyProps,
+    FormProps,
+    FormItemProps,
+    FormInstance,
+    FormItemInstance,
+    InputProps,
+    TextAreaProps,
+    ImageProps,
+    InputNumberProps,
+    LayoutProps,
+    SiderProps,
+    ListProps,
+    ListItemProps,
+    ListItemMetaProps,
+    MessageArgsProps,
+    MenuProps,
+    MenuTheme,
+    SubMenuProps,
+    MenuItemProps,
+    MenuMode,
+    MenuDividerProps,
+    MentionsProps,
+    ModalProps,
+    ModalFuncProps,
+    StatisticProps,
+    NotificationPlacement,
+    PageHeaderProps,
+    PaginationProps,
+    PopconfirmProps,
+    PopoverProps,
+    ProgressProps,
+    RadioProps,
+    RadioChangeEvent,
+    RadioGroupProps,
+    RateProps,
+    ResultProps,
+    RowProps,
+    SelectProps,
+    SkeletonProps,
+    SkeletonButtonProps,
+    SkeletonInputProps,
+    SkeletonImageProps,
+    SkeletonAvatarProps,
+    SkeletonTitleProps,
+    SliderProps,
+    SpaceProps,
+    SpinProps,
+    StepProps,
+    StepsProps,
+    SwitchProps,
+    TableProps,
+    TablePaginationConfig,
+    TableColumnGroupType,
+    TableColumnType,
+    TableColumnProps,
+    TableColumnsType,
+    TransferProps,
+    TreeProps,
+    DirectoryTreeProps,
+    TreeSelectProps,
+    TabsProps,
+    TabPaneProps,
+    TagProps,
+    TimePickerProps,
+    TimeRangePickerProps,
+    TimelineProps,
+    TimelineItemProps,
+    TooltipProps,
+    TypographyProps,
+    UploadProps,
+    UploadListProps,
+    UploadChangeParam,
+    UploadFile
+} from 'ant-design-vue/lib/components.d.ts'
+
+export type * from 'ant-design-vue/lib/form/interface'
+export * from './src/components/BasicForm/types'
+export * from './src/components/BasicLayout/types'
+export * from './src/components/BasicTable/types'
+export * from './src/components/charts/types'
+export * from './src/components/customRangePicker/types'
+export * from './src/components/Descriptions/types'
+export * from './src/components/FooterBar/types'
+export * from './src/components/Loading/types'
+export * from './src/components/loginVerify/types'
+export * from './src/components/Modal/types'
+export * from './src/components/modalSelect/types'
+export * from './src/components/pdfPreview/types'
+export * from './src/components/securityVerificationModal/types'
+export * from './src/components/sIdentify/types'
+export * from './src/components/verificationModal/types'
+export * from './src/components/virtualList/src/types'
+export * from './src/components/Tour/types'
+export * from './src/components/IntlTelInput/types'
+export * from './src/utils/types'
+export * from './src/components/watermark/types'
