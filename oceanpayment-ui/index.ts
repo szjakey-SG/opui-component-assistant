37cfb2619a41f7c69f6c6a0c4a31c12c53c41942
szjakey-SG <szjakey@gmail.com>
2026-06-11 08:58:32 +0800

    Add oceanpayment-ui/index.ts

---
+++ oceanpayment-ui/index.ts
@@ -0,0 +1,10 @@
+// 样式
+// import 'ant-design-vue/dist/antd.css';
+// 暴露图标
+export * from './icons';
+// 暴露组件
+export * from './components';
+// 暴露类型
+export * from './types';
+// 暴露旧的组件
+export * from './oldComponents';
